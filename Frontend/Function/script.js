cdebd873f4de597cd4a972513e5c55a6a6ee9a3d
Richard Liu <richardliux23@gmail.com>
2023-09-01 06:08:08 +0800

    Function expression and declaration

--- Frontend/Function/script.js
+++ Frontend/Function/script.js
@@ -15,9 +15,23 @@ function displayName(name, age){
     const person = `My name is ${name} and I am ${age} years old`;
     return person;
 }
+// Function declaration
+function calAge(birthYear){
+    return 2023 - birthYear;
+}
+
+//Function expression
+const myAge = function (birthYear){
+    return 2023 - birthYear;
+}
+const age1 = myAge(2003);
+console.log("The age is " + age1);
 
 var result = fruitProcessor(5, 0);
 console.log(result);
 
 console.log(fruitProcessor(100, 200));
 console.log(displayName("John", 20));
+
+var age = calAge(1998);
+console.log(age);
