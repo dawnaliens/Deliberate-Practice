56a4834911ba7a5f7d9f61a3e8fbee072dc1f99d
Richard Liu <richardliux23@gmail.com>
2023-09-04 15:35:03 +0800

    practice of for loop

---
+++ Frontend/forLoop/script.js
@@ -0,0 +1,51 @@
+for (let i = 0; i < 10; i++){
+    console.log("This is the " + i + " times hello world")
+}   
+
+const richard = [
+    'Liu',
+    'Richard',
+    1998,
+    'student',
+    ['basketball', 'running'],
+]
+
+const types = [];
+
+for (let i = 0; i < richard.length; i++){
+    console.log(richard[i], typeof(richard[i]));
+
+    // one way
+    // types[i] = typeof richard[i];
+
+    // another way - push
+    types.push(typeof richard[i])
+}
+
+console.log(types)
+
+const years = [2003, 2010, 2023, 2030];
+const ages = [];
+for (let i = 0; i < years.length; i++){
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages)
+
+//continue and break
+// string
+// skip current loop - will out typeof string below
+for (let i = 0; i < richard.length; i++){
+    if(typeof richard[i] !== 'string') {
+        continue;
+    }
+    console.log(richard[i], typeof(richard[i]));
+}
+
+console.log("BREAK------")
+for (let i = 0; i < richard.length; i++){
+    if(typeof richard[i] === 'number') {
+        break;
+    }
+    console.log(richard[i], typeof(richard[i]));
+}
