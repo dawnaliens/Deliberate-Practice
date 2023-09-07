0495528f1027474364e3ae32acf65cb377239e43
Richard Liu <richardliux23@gmail.com>
2023-09-07 13:56:41 +0800

    Implement basic logic

--- Frontend/guessNumber/script.js
+++ Frontend/guessNumber/script.js
@@ -12,8 +12,9 @@
 
 // Add event listener
 
-
-
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 document.querySelector('.check').addEventListener('click', function() {
     // Convert from string to number
     const guess = Number((document.querySelector('.guess').value));
@@ -21,6 +22,28 @@ document.querySelector('.check').addEventListener('click', function() {
 
     if(!guess) {
         document.querySelector('.message').textContent = "No number!"
+    }else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = "Correct Number!"
+    }else if (guess > secretNumber ){
+        if(score > 1){
+            document.querySelector('.message').textContent = "Too high!"
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = "You lost the game"
+            document.querySelector('.score').textContent = 0
+        }
+        
+    }else if (guess < secretNumber) {
+        if(score > 1){
+            document.querySelector('.message').textContent = "Too low!"
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = "You lost the game"
+            document.querySelector('.score').textContent = 0
+        }
+
     }
 });
 
