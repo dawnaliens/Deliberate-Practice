a76d8b90a849b73bae97c5b7791ae9d2f4e4a64c
Richard Liu <richardliux23@gmail.com>
2023-09-07 16:36:57 +0800

    Add guess correct change color function

--- Frontend/guessNumber/script.js
+++ Frontend/guessNumber/script.js
@@ -19,11 +19,17 @@ document.querySelector('.check').addEventListener('click', function() {
     // Convert from string to number
     const guess = Number((document.querySelector('.guess').value));
     console.log(guess, typeof guess);
-
+    // When there is no input
     if(!guess) {
         document.querySelector('.message').textContent = "No number!"
+        // When player wins
     }else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "Correct Number!"
+        
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+        // When guess is too high
     }else if (guess > secretNumber ){
         if(score > 1){
             document.querySelector('.message').textContent = "Too high!"
@@ -33,8 +39,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = "You lost the game"
             document.querySelector('.score').textContent = 0
         }
-        
-    }else if (guess < secretNumber) {
+    }else if (guess < secretNumber) { // When guess is too low 
         if(score > 1){
             document.querySelector('.message').textContent = "Too low!"
             score--;
