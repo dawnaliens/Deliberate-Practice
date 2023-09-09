73e1c25932ccc675aa9436e584e2f8db24d6d16a
Richard Liu <richardliux23@gmail.com>
2023-09-09 15:34:05 +0800

    Finished the whole game

--- Frontend/guessNumber/script.js
+++ Frontend/guessNumber/script.js
@@ -13,6 +13,8 @@
 // Add event listener
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function() {
     // Convert from string to number
     const guess = Number((document.querySelector('.guess').value));
@@ -27,6 +29,11 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
         // When guess is too high
     }else if (guess > secretNumber ){
         if(score > 1){
