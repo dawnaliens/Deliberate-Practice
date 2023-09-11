71c592b50c7f64e534ab9fc4c6be1e6beced1c26
Richard Liu <richardliux23@gmail.com>
2023-09-12 05:26:56 +0800

    OOP for modalWindow

--- Frontend/modalWindow/script.js
+++ Frontend/modalWindow/script.js
@@ -6,10 +6,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-btnsOpenModal[i].addEventListener('click', function(){
+const openModal = function(){
     console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-});
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
     
