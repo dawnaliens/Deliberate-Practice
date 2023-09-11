af8cff499c7418df439bfca169b1074687b0470c
Richard Liu <richardliux23@gmail.com>
2023-09-11 14:33:36 +0800

    Modal window

--- Frontend/modalWindow/script.js
+++ Frontend/modalWindow/script.js
@@ -3,8 +3,13 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].textContent);
+btnsOpenModal[i].addEventListener('click', function(){
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+    
