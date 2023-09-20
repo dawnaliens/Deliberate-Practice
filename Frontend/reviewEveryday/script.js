8ecc00f073adbb42fadc4e4f850daff6ea267043
Richard Liu <richardliux23@gmail.com>
2023-09-20 15:47:52 +0800

    Condition ? exprIfTrue : exprIfFalse

--- Frontend/reviewEveryday/script.js
+++ Frontend/reviewEveryday/script.js
@@ -5,3 +5,20 @@ console.log(age);   // 26
 console.log(++age); // 27
 console.log(age++ + 20);    // 47
 
+
+for (var i = 0; i < name.length; i++) {
+  if (i == 3){
+    continue;
+  }
+  console.log(`I am trying to learn ${name[i]}`)
+}
+
+// condition ? exprIfTrue : exprIfFalse
+
+value1 = 23; 
+value2 = 45;
+value3 = 10;
+var result = value1 > value2 ? true : false;
+var another = value2 > value3 ? true : false;
+console.log(result);    // false
+console.log(another);   // true
