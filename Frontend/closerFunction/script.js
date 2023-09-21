8e64ba92fa86f990e67c4ae30f2148d4370037b0
Richard Liu <richardliux23@gmail.com>
2023-09-21 14:36:33 +0800

    Fixed

--- Frontend/closerFunction/script.js
+++ Frontend/closerFunction/script.js
@@ -23,3 +23,23 @@ createBooking('WEQ789', 3);
 
 // Another way
 createBooking('123456', undefined, 2003)
+
+const flight = 'CZ303';
+const richard = {
+    name: 'Richard',
+    passport: 6456465
+}
+
+const checkIn = function(flightNum, passenger){
+    flightNum = 'CZ100';
+    passenger.name = 'Mr. ' + passenger.name;
+    if (passenger.passport === 6456465){
+        alert('Check in');
+    }else{
+        alert('Wrong Passport!')
+    }
+}
+
+checkIn(flight, richard);
+console.log(flight)
+console.log(richard)
