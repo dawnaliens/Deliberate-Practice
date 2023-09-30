ef780d3fe495bbc8581a7b8046ff37f2427e1cce
Richard Liu <richardliux23@gmail.com>
2023-09-30 15:07:03 +0800

    More closures

--- Frontend/closerFunction/script.js
+++ Frontend/closerFunction/script.js
@@ -211,3 +211,43 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+
+let f;
+
+const g = function(){
+    const a = 23;
+    f = function(){
+        console.log(a * 2);
+    }
+}
+
+const h = function(){
+    const b = 777;
+    f = function(){
+        console.log(b * 2);
+    }
+}
+
+g();
+f();
+console.dir(f);
+
+//Reassign f function
+h();
+f();
+console.dir(f);
+
+const boardPassengers = function(n ,wait){
+    const perGroup = n / 3;
+
+    setTimeout(function(){
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`)
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds.`)
+}
+
+boardPassengers(180,3);
+
+
