0523fa0c729f1d0ab4d7d8f9af7f6da01d4225f9
Richard Liu <richardliux23@gmail.com>
2023-10-09 05:06:39 +0800

    At function

--- Frontend/ArrayPart/script.js
+++ Frontend/ArrayPart/script.js
@@ -75,7 +75,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 // console.log(arr.slice(2));
 // console.log(arr.slice(2, 4));
 // console.log(arr.slice(-2));
@@ -90,18 +90,30 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 // console.log(arr);
 
 // Reverse
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-// Concat
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// // Concat
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
-// Join
-console.log(letters.join(' - '))
+// // Join
+// console.log(letters.join(' - '))
+
+const arr = [23, 45 ,100];
+console.log(arr[0]);
+console.log(arr.at(1));
+
+console.log(arr[arr.length-1]);  // last element
+console.log(arr.slice(-1)[0]);  //100
+console.log(arr.at(-1));  // 100
+console.log(arr.at(-2));  // 45
+
+console.log('Richard'.at(0)) // R
+
 
 
 
