9d15de2affb040d47c43262740fe1c95a9ecfa45
Richard Liu <richardliux23@gmail.com>
2023-10-22 17:59:35 +0800

    Testing

--- Frontend/Bankist-App/script.js
+++ Frontend/Bankist-App/script.js
@@ -62,15 +62,17 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function(movements){
-  movements.forEach(function(mov, i ){
+  movements.forEach(function(mov, i){
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
     const html = `
       <div class="movements__row">
         <div class="movements__type
-        movements__type--deposit">2 deposit</div>
+        movements__type--${type}">${i + 1} ${type}</div>
         <div class="movements__value">${mov}</div>
       </div>
     `;
   })
+  .insertAdjacentHTML
 }
 displayMovements(account1.movements);
 
